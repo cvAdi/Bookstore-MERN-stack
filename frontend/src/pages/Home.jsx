bab9a0d739839dd23ac0c87b550bb9f2be82b6a3
Adithya <adithyacv27@gmail.com>
2025-03-13 23:20:24 +0530

    13-03-v2
    show book changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import Spinner from '../components/Spinner'
 import { Link } from 'react-router-dom';
-// import { AioutlineEdit } from 'react-icons/ai';
+import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
 
@@ -13,7 +13,7 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(false);
+    setLoading(true);
     axios.get('http://localhost:5555/books')
       .then((response) => {
         setBooks(response.data.data)
@@ -22,8 +22,8 @@ const Home = () => {
       .catch((error) => {
         console.log(error);
         setLoading(false);
-      })
-  })
+      });
+  }, [])
 
   return (
     <div className='p-4'>
@@ -61,9 +61,9 @@ const Home = () => {
                       <BsInfoCircle className='text-2xl text-green-800' />
                     </Link>
                     <Link to={`/books/edit/${book._id}`}>
-                     
+                     <AiOutlineEdit className='text-2xl text-yellow-600'/>
                     </Link>
-                    <Link to={`/books/edit/${book._id}`}>
+                    <Link to={`/books/delete/${book._id}`}>
                       <MdOutlineDelete className='text-2xl text-red-600' />
                     </Link>
                   </div>
